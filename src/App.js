8d49c31e407a278b24e1f317612ddbf73f78dd6e
Steven Zuluaga <steven000069@hotmail.com>
2022-11-11 00:47:39 -0500

    Agregando rutas a la app web

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
 import News from './components/News/News'
+
 
 const App = () => {
   return (
     <div>
-      <Home />
-      <News/>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/news' element={<News />} />
+      </Routes>
     </div>
   )
 }
