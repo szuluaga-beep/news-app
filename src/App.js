9111603273e5ff4bfa55c9f034d1612bcf971882
Steven Zuluaga <steven000069@hotmail.com>
2022-11-16 23:07:15 -0500

    Agregando componente para error de no encontrado

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import News from './components/News/News'
 import 'bootstrap/dist/css/bootstrap.css';
 import NewDetails from './components/News/NewDetails/NewDetails'
 import FormNew from './components/FormNews/FormNew'
+import NotFound from './components/NotFound/NotFound'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path='formNew' element={ <FormNew/>} />
         <Route path='/news' element={<News />} />
         <Route path='/news/:id' element={<NewDetails />} />
+        <Route path='*' element={<NotFound />}/>
       </Routes>
     </div>
   )
